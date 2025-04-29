dbc03b9f70b6769fd5677d0c8bdb5c6c9a6931ad
YounesDjelloul <younesdjelloul14@gmail.com>
2025-04-30 04:04:59 +0800

    we should be able to fulfill a one-time plan with multiple deposits

--- src/allocator.test.ts
+++ src/allocator.test.ts
@@ -134,4 +134,49 @@ describe('Deposit Allocator', () => {
 
         expect(result).toEqual(expectedResult);
     });
+
+    it('should deactivate the one-time plan even if it is fulfilled via multiple deposits', () => {
+        const portfolios: Portfolio[] = [
+            {id: 'p1', name: 'High risk'},
+            {id: 'p2', name: 'Retirement'}
+        ];
+
+        const depositPlans: DepositPlan[] = [
+            {
+                id: 'dp1',
+                type: PlanType.ONE_TIME,
+                allocations: [
+                    {portfolioId: 'p1', amount: 10000},
+                    {portfolioId: 'p2', amount: 500}
+                ],
+                isActive: true,
+                totalAmount: 10500
+            },
+            {
+                id: 'dp2',
+                type: PlanType.MONTHLY,
+                allocations: [
+                    {portfolioId: 'p1', amount: 0},
+                    {portfolioId: 'p2', amount: 100}
+                ],
+                isActive: true,
+                totalAmount: 100
+            }
+        ];
+
+        const deposits: Deposit[] = [
+            {id: 'd1', amount: 5250, referenceCode: 'ref123', timestamp: new Date('2025-04-10')},
+            {id: 'd1', amount: 5250, referenceCode: 'ref123', timestamp: new Date('2025-04-10')},
+            {id: 'd1', amount: 100, referenceCode: 'ref123', timestamp: new Date('2025-04-10')}
+        ];
+
+        const result = allocateDeposits(portfolios, depositPlans, deposits);
+
+        const expectedResult: PortfolioAllocation[] = [
+            {portfolioId: 'p1', amount: 10000},
+            {portfolioId: 'p2', amount: 600}
+        ];
+
+        expect(result).toEqual(expectedResult);
+    });
 });
