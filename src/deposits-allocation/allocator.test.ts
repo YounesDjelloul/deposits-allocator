b7b81cad826e44571693759847eaa5e093e7dfed
YounesDjelloul <younesdjelloul14@gmail.com>
2025-05-02 03:42:09 +0800

    distribute normally if only some plans have zeros only test

--- src/deposits-allocation/allocator.test.ts
+++ src/deposits-allocation/allocator.test.ts
@@ -402,4 +402,48 @@ describe('Deposit Allocator', () => {
 
         expect(result).toEqual(expectedResult);
     });
+
+    it('should distribute deposits normally on portfolios if only some plans has only 0s', () => {
+        const portfolios: Portfolio[] = [
+            {id: 'p1', name: 'High risk'},
+            {id: 'p2', name: 'Retirement'}
+        ];
+
+        const depositPlans: DepositPlan[] = [
+            {
+                id: 'dp1',
+                type: PlanType.ONE_TIME,
+                allocations: [
+                    {portfolioId: 'p1', amount: 0},
+                    {portfolioId: 'p2', amount: 0}
+                ],
+                isActive: true,
+                totalAmount: 0
+            },
+            {
+                id: 'dp2',
+                type: PlanType.MONTHLY,
+                allocations: [
+                    {portfolioId: 'p1', amount: 0},
+                    {portfolioId: 'p2', amount: 100}
+                ],
+                isActive: true,
+                totalAmount: 100
+            }
+        ];
+
+        const deposits: Deposit[] = [
+            {id: 'd1', amount: 100, referenceCode: 'ref123', timestamp: new Date('2025-04-10')},
+            {id: 'd2', amount: 100, referenceCode: 'ref123', timestamp: new Date('2025-04-10')},
+        ];
+
+        const result = allocateDeposits(portfolios, depositPlans, deposits);
+
+        const expectedResult: PortfolioAllocation[] = [
+            {portfolioId: 'p1', amount: 0},
+            {portfolioId: 'p2', amount: 200}
+        ];
+
+        expect(result).toEqual(expectedResult);
+    });
 });
